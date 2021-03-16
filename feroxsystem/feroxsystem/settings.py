767f783e647e30563dcb507a70f5575d49c1247f
minhdang741 <minhdang741@github.com>
2021-03-16 13:02:12 +0700

    Merge from origin

--- feroxsystem/feroxsystem/settings.py
+++ feroxsystem/feroxsystem/settings.py
@@ -73,20 +73,14 @@ WSGI_APPLICATION = 'feroxsystem.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'mydatabase',
-    }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
